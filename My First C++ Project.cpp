84a6db4562c22a5933def0bc29cf2f3626ef2ddc
Victoria Dathan <vdathan18@gmail.com>
2021-09-21 18:08:45 -0400

    descriptive comment for code changes

---
+++ My First C++ Project.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // this is my business card by Victoria Dathan//
+
+    string name = "Victoria Dathan" ;
+    string street_address = "6279 Drew Drive";
+    string phone_number = "7577563586";
+    string email_address = "vdathan18@gmail.com";
+
+
+    cout << "Magic Fingers"<< endl;
+    cout << name << endl;
+    cout << street_address << endl;
+    cout << phone_number << endl;
+    cout << email_address << endl << endl;
+
+
+    cout << "Magic Fingers" << endl;
+    cout << name << endl;
+    cout << street_address << endl;
+    cout << phone_number << endl;
+    cout << email_address << endl << endl;
+
+
+    cout << "Magic Fingers" << endl;
+    cout << name << endl;
+    cout << street_address << endl;
+    cout << phone_number << endl;
+    cout << email_address << endl;
+
+    cout << "Magic Fingers" << endl;
+    cout << name << endl;
+    cout << street_address << endl;
+    cout << phone_number << endl;
+    cout << email_address << endl << endl;
+
+    cout << "Magic Fingers" << endl;
+    cout << name << endl;
+    cout << street_address << endl;
+    cout << phone_number << endl;
+    cout << email_address << endl << endl;
+
+    cout << "Magic Fingers" << endl;
+    cout << name << endl;
+    cout << street_address << endl;
+    cout << phone_number << endl;
+    cout << email_address << endl << endl;
+
+    cout << "Magic Fingers" << endl;
+    cout << name << endl;
+    cout << street_address << endl;
+    cout << phone_number << endl;
+    cout << email_address << endl << endl;
+
+    cout << "Magic Fingers" << endl;
+    cout << name << endl;
+    cout << street_address << endl;
+    cout << phone_number << endl;
+    cout << email_address << endl << endl;
+
+}
